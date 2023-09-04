4e1c90e6e79b8bcbe463489de9c8f600d34ed981
Tim Mueller <trmueller5@gmail.com>
2023-09-03 21:50:18 -0500

    set up database

---
+++ server/migrations/versions/bf07a25b17f4_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: bf07a25b17f4
+Revises: 
+Create Date: 2023-09-03 21:40:25.733282
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bf07a25b17f4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -3,4 +3,38 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    # _password_hash = db.Column(db.String, nullable=False)
+
+    goals = db.relationship('Goal', backref='user', cascade='all, delete-orphan')
+
+class Goal(db.Model, SerializerMixin):
+    __tablename__ = 'goals'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    habits = db.relationship('Habit', backref='goal', cascade='all, delete-orphan')
+    tasks = db.relationship('Task', backref='goal', cascade='all, delete-orphan')
+
+
+class Habit(db.Model, SerializerMixin):
+    __tablename__ = 'habits'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    goal_id = db.Column(db.Integer, db.ForeignKey("goals.id"))
+
+
+class Task(db.Model, SerializerMixin):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    goal_id = db.Column(db.Integer, db.ForeignKey("goals.id"))

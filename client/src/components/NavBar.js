194e021dfcaf475f81ec389a9eb301d18e1648ca
Tim Mueller <trmueller5@gmail.com>
2023-09-20 16:07:16 -0500

    navbar style

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -38,8 +38,8 @@ function NavBar({ inventory, navDisplay }) {
     return (
         <div className={navDisplay}>
             <div className='grid grid-cols-2 grid-rows-2 max-w-sm m-auto'>
-                <button onClick={goCreateGoal} className='border-solid border-black border-2 px-4 m-1 max-h-14 h-12 bg-blue-500 rounded-full shadow-lg shadow-gray-600 hover:rounded-full hover:bg-blue-700 hover:border-gray-50 hover:text-gray-50 hover:shadow-white hover:shadow-inner'> Create New Goal </button>
-                <button onClick={viewCompleted} className='border-solid border-black border-2 px-4 m-1 max-h-14 h-12 bg-blue-500 rounded-full shadow-lg shadow-gray-600 hover:rounded-full hover:bg-blue-700 hover:border-gray-50 hover:text-gray-50 hover:shadow-white hover:shadow-inner'>Completed Goals</button>
+                <button onClick={goCreateGoal} className='border-solid border-black border-2 px-4 m-1 max-h-14 h-12 bg-blue-500 rounded-full shadow-lg shadow-gray-600 overflow-hidden hover:rounded-full hover:bg-blue-700 hover:border-gray-50 hover:text-gray-50 hover:shadow-white hover:shadow-inner'> Create New Goal </button>
+                <button onClick={viewCompleted} className='border-solid border-black border-2 px-4 m-1 max-h-14 h-12 bg-blue-500 rounded-full shadow-lg shadow-gray-600 overflow-hidden hover:rounded-full hover:bg-blue-700 hover:border-gray-50 hover:text-gray-50 hover:shadow-white hover:shadow-inner'>Completed Goals</button>
                 <button onClick={handleLogOut} className='border-solid border-black border-2 px-4  col-span-2 max-h-14 w-32 m-auto bg-blue-500 rounded-full shadow-lg shadow-gray-600 hover:rounded-full hover:bg-blue-700 hover:border-gray-50 hover:text-gray-50 hover:shadow-white hover:shadow-inner'> Log Out </button>
             </div>
             <div className='flex flex-col items-center'>

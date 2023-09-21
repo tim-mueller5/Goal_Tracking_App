cc69d12e95807160d0fa8b24cddbb292dc2318fa
Tim Mueller <trmueller5@gmail.com>
2023-09-21 15:57:39 -0500

    fix no goals message to pop up properly

--- client/src/components/GoalList.js
+++ client/src/components/GoalList.js
@@ -24,14 +24,15 @@ function GoalList({ currentGoal, setCurrentGoal, inventory, setInventory}) {
             } else{return null}
         })
     }
-    
+
+    const listEmpty = goalsToDisplay.filter(goal => goal !== null)
 
     return (
         <div className="relative overflow-auto min-h-screen font-display ">
             <p className="fixed top-56 left-1/2 bg-slate-500">{newItemMessage}</p>
             <div className="w-3/4 float-left">
                 {(user != null)? goalsToDisplay : "No user"}
-                <p className="m-4">{goalsToDisplay.length >= 1 ? null : "You have no goals!  Create a new goal!"}</p>
+                <p className="m-4">{listEmpty.length === 0 ? "You have no goals!  Create a new goal!" : null}</p>
             </div>
             <div className="float-right w-1/4">
                 <div className="border-black border-2 h-64 flex flex-col items-center">

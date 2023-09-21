87c0638ae016d585743d638191fd215ac4e8232d
Tim Mueller <trmueller5@gmail.com>
2023-09-21 08:34:58 -0500

    checking style touch up

--- client/src/components/HabitCheckIn.js
+++ client/src/components/HabitCheckIn.js
@@ -5,7 +5,7 @@ function HabitCheckIn ({ checkin }) {
 
     const date = new Date(checkin.date).toLocaleDateString()
     const [isComleted, setIsComleted]  = useState(checkin.completed)
-    let design = "border-solid border-black border-2 text-sm rounded-full flex mx-1 px-2 pt-1 font-thin font-display text-center"
+    let design = "border-solid border-black border-2 text-sm rounded-full flex m-auto px-2 font-thin font-display"
 
     function handleCompelete() {
         fetch(`/checkins/${checkin.id}`, {
@@ -18,15 +18,15 @@ function HabitCheckIn ({ checkin }) {
         setIsComleted(!isComleted)
     }
     if(isComleted === true){
-        design = "border-solid border-black border-2 text-sm rounded-full flex mx-1 px-2 pt-1 font-thin font-display text-center bg-green-300"
+        design = "border-solid border-black border-2 text-sm rounded-full flex m-auto px-2 font-thin font-display bg-green-300"
     }
 
     return (
         <div className={design}>
-            <p className="pt-1">{date}</p>
+            <p className=" m-auto">{date}</p>
             {isComleted ? 
-            <p className="px-1 pt-1">Completed</p>
-            : <button onClick={handleCompelete} className='border-solid border-black border-2 px-4 m-1 bg-blue-500 rounded-full shadow-lg shadow-gray-600 hover:rounded-full hover:bg-blue-700 hover:border-gray-50 hover:text-gray-50 hover:shadow-white hover:shadow-inner'>Complete</button>}
+            <p className="px-1  m-auto">Completed</p>
+            : <button onClick={handleCompelete} className='border-solid border-black border-2 px-2 m-1 bg-blue-500 rounded-full shadow-lg shadow-gray-600 hover:rounded-full hover:bg-blue-700 hover:border-gray-50 hover:text-gray-50 hover:shadow-white hover:shadow-inner'>Complete</button>}
             <p className="text-lg">&#8594;</p>
         </div>
     )
